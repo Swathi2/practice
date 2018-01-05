3609a6c6678af9a74615deca38ec134c13e44cc1
Swathi <swathireddy.patlolla@gmail.com>
2018-01-05 09:00:57 -0500

    Add files via upload

---
+++ sublime/scripts/buyCtrl.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+angular.module('sublimeApp')
+.controller('buyCtrl', function ($scope) {
+    $scope.hidelicenses = false;
+    $scope.licenseKey = true;
+    $scope.disabled = true;
+    $scope.bsnssLicenseFunc = function () {
+        $scope.disabled = false;
+        $scope.hidelicenses = true;
+        $scope.licenseKey = false;
+    }
+    $scope.selfLicenseFunc = function () {
+        $scope.hidelicenses = false;
+        $scope.licenseKey = true;
+    }
+})
---
+++ sublime/scripts/forumCtrl.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+angular.module('sublimeApp')
+.controller('forumCtrl', function ($scope, $http) {
+     $http.get('forumData.json')
+   .then(function (res) {
+       $scope.forumData = res.data;
+   });
+})
